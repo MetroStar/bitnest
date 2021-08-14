8b67a9801d352351c42618ed5f239e42ce5f2833
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-08-13 20:23:45 -0400

    Full parsing rules

--- bitnest/core.py
+++ bitnest/core.py
@@ -1,20 +1,47 @@
 from bitnest.field import Struct, Field, Union, Vector
 
 
-def _realize_datatypes(struct):
-    datatypes, conditions = (), []
+def _realize_datatype(struct):
+    datatype, conditions = [
+        struct,
+    ], []
+
+    for _condition in struct.conditions:
+        conditions.append((struct, _condition))
+
     for field in struct.fields:
         if isinstance(field, Union):
+            union_datatype = [field.__class__]
             for union_struct in field.structs:
-                datatypes, conditions = _realize_datatypes(union_struct)
+                _datatype, _conditions = _realize_datatype(union_struct)
+                for _struct, _condition in _conditions:
+                    conditions.append(
+                        (
+                            (field.__class__, union_struct, _struct),
+                            (field.__class__, union_struct, _condition),
+                        )
+                    )
+                union_datatype.append(_datatype)
+            datatype.append(union_datatype)
         elif isinstance(field, Vector):
-            datatypes, conditions = _realize_datatypes(field.klass)
+            _datatype, _conditions = _realize_datatype(field.klass)
+            for _struct, _condition in _conditions:
+                conditions.append(
+                    (
+                        (field.__class__, field.klass, _struct),
+                        (field.__class__, field.klass, _condition),
+                    )
+                )
+            datatype.append((field.__class__, field.klass, _datatype))
         elif isinstance(field, Field):
-            pass
+            datatype.append(field.__class__)
         elif issubclass(field, Struct):
-            datatypes, conditions = _realize_datatypes(field)
-    return datatypes, conditions
+            _datatype, _conditions = _realize_datatype(field)
+            for _struct, _condition in _conditions:
+                conditions.append(((struct, _struct), (struct, _condition)))
+            datatype.append(_datatype)
+    return tuple(datatype), conditions
 
 
-def realize_datatypes(root_class):
-    return _realize_datatypes(root_class)
+def realize_datatype(root_class):
+    return _realize_datatype(root_class)
--- tests/test_core.py
+++ tests/test_core.py
@@ -3,11 +3,11 @@ import pytest
 from models.simple import MILSTD_1553_Message
 from models.chapter10 import MILSTD_1553_Data_Packet_Format_1
 
-from bitnest.core import realize_datatypes
+from bitnest.core import realize_datatype
 
 
 @pytest.mark.parametrize(
     "struct", [MILSTD_1553_Message, MILSTD_1553_Data_Packet_Format_1]
 )
 def test_realize_paths(struct):
-    realize_datatypes(struct)
+    datatype, conditions = realize_datatype(struct)
